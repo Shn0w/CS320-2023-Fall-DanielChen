545dbaf4b89367e23d00c3e7863caa0ce14b246d
Shn0w <danielchen3575@gmail.com>
2023-09-19 17:43:27 -0400

    updated

--- assigns/assign1/MySolution/assign1-2.ml
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -1,20 +1,36 @@
-#use "./../classlib/OCaml/MyOCaml.ml";;
+#use "CS320-2023-Fall-DanielChen/classlib/OCaml/MyOCaml.ml";;
 
-let delete(cs: string): string = 
-  string_init(string_length(cs) + 1)(
-    fun i -> if i = string_length(cs)-1 then "" else string_get_at cs (i-1)
-  )
+let check a b: bool =
+  a>b
+;;
+
+let solve cs1 cs2: string = 
+  string_make_fwork
+  (fun work -> 
+    string_foreach cs1
+    (fun a -> string_foreach cs2
+    (fun b -> if check a b then work a else work b)))
 ;;
 
 let string_merge(cs1: string)(cs2: string): string =
+  solve cs1 cs2
+;;
+
+(*
   let rec solve(a: string)(b: string)(out: string) string =
-    if string_length(a)=0
-      then string_snoc(out)(b)
-    else
-    if string_length(b)=0
-      then string_snoc(out)(a)
-    else
-    if chr(string_head(a)) >= chr(string_head(b))
+    if string_length a=0 && string_length b = 0 
+      then out
+    else if string_length(a)=0
+      then 
+      solve(a)
+      (delete(b))
+      (string_snoc(out)(string_head(b)))
+    else if string_length(b)=0
+      then 
+      solve (delete(a))
+      (b)
+      (string_snoc(out)(string_head(a)))
+    else if string_head(a) >= string_head(b)
       then 
       solve(a)
       (delete(b))
@@ -25,6 +41,4 @@ let string_merge(cs1: string)(cs2: string): string =
       (b)
       (string_snoc(out)(string_head(a)))
 in solve cs1 cs2 ""
-;;
-
-(*string_merge("12")("34")*)
+*)
